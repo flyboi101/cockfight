e72753ad645ab4af8071c630232e6cfe79fe80dd
mainman100 <85406613+flyboi101@users.noreply.github.com>
2024-12-21 12:24:01 -0500

    Update BottomIcons.tsx

--- src/games/ExampleGame/BottomIcons.tsx
+++ src/games/ExampleGame/BottomIcons.tsx
@@ -8,7 +8,7 @@ const BottomIcons: React.FC = () => {
   const [isMobile, setIsMobile] = useState<boolean>(false);
 
   const iconLinks = [
-    { src: icon1, url: '' },
+    { src: icon1, url: 'https://x.com/Degen_CF' },
     { src: icon2, url: '' },
     { src: icon3, url: '' },
   ];
